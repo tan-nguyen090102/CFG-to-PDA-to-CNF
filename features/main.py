b1a2c7e01c6a4f9d1644e2c1173c0974c9b47489
tan-nguyen090102 <134823646+tan-nguyen090102@users.noreply.github.com>
2023-11-21 23:57:47 -0600

    Create conversion file from CFG to CNF.

---
+++ features/main.py
@@ -0,0 +1,16 @@
+"""Module to convert CFG to PDA"""
+from cfg_to_cnf import CFGToCNF
+from cfg_to_pda import CFGToPDA
+
+pda_conversion = CFGToPDA()
+cnf_conversion = CFGToCNF()
+
+if __name__ == "__main__":
+    # Convert CFG to PDA
+    # productions = pda_conversion.storing_cfg()
+    # nodes = pda_conversion.storing_pda(production_list=productions)
+    # pda_conversion.graphing_pda(node_list=nodes)
+
+    # Convert CFG to CNF
+    productions = cnf_conversion.storing_cfg()
+    cnf_conversion.convert_to_cnf(production_list=productions)
